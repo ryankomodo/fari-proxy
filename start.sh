e020fde3b1bf1b77b90a94899c386d67d914e23c
leviathan0992@gmail.com <leviathan0992@gmail.com>
2018-12-12 07:30:49 +0800

    update start.sh

--- start.sh
+++ start.sh
@@ -2,13 +2,19 @@
 
 if [ "$1" = "client" ]
 then
+  sudo mkdir -p /root/proxy/
   sudo cp fari-client.service /etc/systemd/system/
+  sudo cp fari-client /root/proxy/
+  sudo cp .client.json /root/proxy/
   systemctl enable fari-client
   systemctl start fari-client
   systemctl status fari-client -l
 elif [ "$1" = "server" ]
 then
+  sudo mkdir -p /root/proxy/
   sudo cp fari-server.service /etc/systemd/system/
+  sudo cp fari-server /root/proxy/
+  sudo cp .server.json /root/proxy/
   systemctl enable fari-server
   systemctl start fari-server
   systemctl status fari-server -l
